eb6ea0e7a726e2f4632f391dee713740f1c0cf65
OlivierCodol <codol.olivier@gmail.com>
2021-08-04 08:25:58 -0400

    added blogpost directory structure

--- deploy-html.py
+++ deploy-html.py
@@ -16,34 +16,47 @@ header = fp.read()
 fp.close()
 
 fp = open(template_dir + '/navigation.html', 'r')
-navigation = fp.read()
+navgtn = fp.read()
 fp.close()
 
 fp = open(template_dir + '/footer.html', 'r')
 footer = fp.read()
 fp.close()
 
+files = [os.path.join(dp, f) for dp, dn, fn in os.walk(os.path.expanduser(src_html_dir)) for f in fn]
+ix = len(src_html_dir) + 1
 
+for filepath in files:
 
-for file in os.listdir(src_html_dir):
-    source_file = src_html_dir + "/" + file
+    file = filepath[ix:]
+    source_file = os.path.join(src_html_dir, file)
     if file == "index-content.html":
         file = "index.html"
-    compil_file = compil_dir + "/" + file  # .replace('.md', '.html')
+    compil_file = os.path.join(compil_dir, file)  # .replace('.md', '.html')
+
     navitem_old_pre = """li class="nav-item"><a class="nav-link" href=" """
     navitem_old_suf = """ " data-no="1">"""
     navitem_old = navitem_old_pre[:-1] + file + navitem_old_suf[1:]
     navitem_new_pre = """li class="nav-item selected"><a class="nav-link" href=" """
     navitem_new = navitem_new_pre[:-1] + file + navitem_old_suf[1:]
+
+    relative_path = "../" * file[:file.rfind("\\") + 1].count("\\")
+    href_flag = """href=" """[:-1]
+    src_flag = """src=" """[:-1]
+
     # pypandoc.convert_file(source_file, 'html', outputfile=compil_file)
+    os.makedirs(os.path.dirname(compil_file), exist_ok=True)
     shutil.copy(source_file, compil_file)
     print(source_file + ' -> ' + compil_file)
 
-    yn = navigation.find(navitem_old)
-    navigation_new = navigation.replace(navitem_old, navitem_new)
     fp = open(compil_file, 'r+')
     source = fp.read()
+    header_new = header.replace(href_flag + "css", href_flag + relative_path + "css")
+    navgtn_new = navgtn.replace(href_flag, href_flag + relative_path).replace(src_flag, src_flag + relative_path)
+    source_new = source.replace(href_flag, href_flag + relative_path).replace(src_flag, src_flag + relative_path)
+    footer_new = footer.replace(href_flag, href_flag + relative_path).replace(src_flag, src_flag + relative_path)
+    navgtn_new = navgtn_new.replace(navitem_old, navitem_new)
     fp.seek(0)
-    page = header + navigation_new + source + footer
+    page = header_new + navgtn_new + source_new + footer_new
     fp.write(page)
     fp.close()
